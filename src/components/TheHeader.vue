4d24a273152450824c1c99876aa22d295f378e82
Joseph Grant <joseph@halfcab.com.au>
2021-02-25 16:25:59 +0800

    WIP on mobile menu

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,7 +8,7 @@
       class="hover:text-blue-500 transition duration-200 ease-in-out"
       >{{ $static.metadata.siteName }}</g-link
     >
-    <nav class="flex items-center gap-x-6">
+    <nav class="hidden md:flex items-center gap-x-6">
       <g-link
         class="hover:text-blue-500 transition duration-200 ease-in-out"
         to="/"
@@ -30,6 +30,35 @@
         >Contact</g-link
       >
     </nav>
+
+    <div>
+      <TheHamburger class="w-6 h-6" @clicked="toggleMenuIsOpen()" />
+      <nav
+        class="flex flex-col gap-y-6 md:hidden fixed top-0 w-screen bg-blue-400 h-full max-w-xl transition duration-200 ease-in-out transform"
+        :class="menuIsOpen ? 'translate-x-0' : 'translate-x-full'"
+      >
+        <g-link
+          class="hover:text-blue-500 transition duration-200 ease-in-out"
+          to="/"
+          >Home</g-link
+        >
+        <g-link
+          class="hover:text-blue-500 transition duration-200 ease-in-out"
+          to="/about"
+          >About</g-link
+        >
+        <g-link
+          class="hover:text-blue-500 transition duration-200 ease-in-out"
+          to="/blog"
+          >Blog</g-link
+        >
+        <g-link
+          class="hover:text-blue-500 transition duration-200 ease-in-out"
+          to="/contact"
+          >Contact</g-link
+        >
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -40,3 +69,23 @@ query {
   }
 }
 </static-query>
+
+<script>
+import TheHamburger from "@/components/svg/TheHamburger";
+
+export default {
+  components: {
+    TheHamburger,
+  },
+  data() {
+    return {
+      menuIsOpen: false,
+    };
+  },
+  methods: {
+    toggleMenuIsOpen() {
+      this.menuIsOpen = !this.menuIsOpen;
+    },
+  },
+};
+</script>

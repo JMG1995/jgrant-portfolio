d07c032edd9da681ff02eca529fc062ae25e2869
Joseph Grant <joseph@halfcab.com.au>
2021-03-29 13:35:15 +0800

    add favicon

--- src/main.js
+++ src/main.js
@@ -16,6 +16,25 @@ export default function(Vue, { router, head, isClient }) {
 			rel: 'stylesheet',
 			href:
 				'https://fonts.googleapis.com/css2?family=Karla:wght@400;500;700&family=Rubik:wght@400;500;600&display=swap',
+		},
+		{
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			href: '/favicon/apple-touch-icon.png',
+		},
+		{
+			rel: 'icon',
+			sizes: '32x32',
+			href: '/favicon/favicon-32x-32.png',
+		},
+		{
+			rel: 'icon',
+			sizes: '16x16',
+			href: '/favicon/favicon-16x16.png',
+		},
+		{
+			rel: 'manifest',
+			href: '/favicon/site.webmanifest',
 		}
 	);
 }
